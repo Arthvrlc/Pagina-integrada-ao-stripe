e8460f0d29c852ba54e5efd9e84d44b066fe053c
Arthur Lobato <lobatoarthurc@outlook.com>
2022-11-14 15:02:24 -0300

    novo commit

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,12 +8,38 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const session = useSession();
+    const user = session.data?.user;
+
+    async function handleSubscribe() {
+        try {
+            
+            if (!session) {
+                signIn('github')
+                return;
+            }
+            console.log('user INFOOOOOOo', user)
+            const response = await api.post('/subscribe', {
+                user
+            });
+
+            const { sessionId } = response.data;
+            console.log(response.data)
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({ sessionId });
+        } catch (err) {
+            alert(err.message);
+        }
+    }
     return (
         <button
-        type="button"
-        className={styles.subscribeButton}
+            type="button"
+            className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
-        Subscribe now 
+            Subscribe now
         </button>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,23 +20,23 @@ export default function Home({ product }: HomeProps) {
 
   return (
     <>
-    <Head>
-    <title>Home | ig.news</title>
-    </Head>
-    
-    <main className={styles.contentContainer}>
-    <section className={styles.hero}>
-      <span>🤝 Hey, welcome </span>
-      <h1>News about the <span>React</span> world.</h1>
-      <p>
-        Get access to all the publications <br/>
-        <span>for {product.amount} month</span>
-      </p>
-      <SubscribeButton priceId={product.priceId}/>
-    </section>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
 
-    <img src="/images/avatar.svg" alt="Girl coding" />
-    </main>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🤝 Hey, welcome </span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
@@ -44,18 +44,18 @@ export default function Home({ product }: HomeProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1LmkezHwUaUiBa1J5moCDWE7')
-    
+
 
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
-    }).format(price.unit_amount / 100) ,
+    }).format(price.unit_amount / 100),
   };
 
   return {
-    props:{
+    props: {
       product,
     },
     revalidate: 60 * 60 * 24, //24horas
